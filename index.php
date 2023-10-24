7385e837c2d20cc04d219c6e6409a2b5d2aced4b
William Silva Sebastião <52112779+bywilliams@users.noreply.github.com>
2023-10-24 09:30:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('./controllers/UserController.php');
+require_once('controllers/UserController.php');
 
 // Define um array associativo para mapear rotas e métodos HTTP
 $routes = array(
