703334afab1f237c453ce7a809c2fa6472cd0f1b
William Silva Sebastião <52112779+bywilliams@users.noreply.github.com>
2023-10-25 09:57:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 require_once('controllers/UserController.php');
+require_once('controllers/DoctorController.php');
 
-// Define um array associativo para mapear rotas e métodos HTTP
+/// Array associativo das rotas
 $routes = array(
     'GET' => array(
-        '/' => array('controller' => 'UserController', 'action' => 'index'),
-        'dashboard' => array('controller' => 'UserController', 'action' => 'dashboard'),
-        'usuarios' => array('controller' => 'UserController', 'action' => 'listUsers'),
-        'logout' => array('controller' => 'UserController', 'action' => 'logout')
+        '/' => 'UserController@index',
+        'dashboard' => 'UserController@dashboard',
+        'usuarios' => 'UserController@listUsers',
+        'doctors' => 'DoctorController@index',
+        'logout' => 'UserController@logout'
     ),
     'POST' => array(
-        'login' => array('controller' => 'UserController', 'action' => 'validarUser'),
-        'users/create' => array('controller' => 'UserController', 'action' => 'create'),
-        'users/update' => array('controller' => 'UserController', 'action' => 'update'),
-        'users/delete' => array('controller' => 'UserController', 'action' => 'delete')
+        'login' => 'UserController@validarUser',
+        'users/create' => 'UserController@create',
+        'users/update' => 'UserController@update',
+        'users/delete' => 'UserController@delete'
     )
 );
 
@@ -24,20 +26,33 @@ $requestMethod = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
 $route = filter_input(INPUT_GET, 'route') ?? '/';
 
 // Verifica o tipo de requisição e se a rota existe no array de rotas
-if (array_key_exists($route, $routes[$requestMethod]) && array_key_exists($requestMethod, $routes)) {
+if (array_key_exists($requestMethod, $routes) && array_key_exists($route, $routes[$requestMethod])) {
     
-    $controllerName = $routes[$requestMethod][$route]['controller'];
-    $action = $routes[$requestMethod][$route]['action'];
+    // Dividi a string controller@action em 2 partes e armmazenas em 2 variáveis
+    list($controllerName, $action) = explode('@', $routes[$requestMethod][$route]);
 
-    // Cria o objeto do controlador com base no nome do controlador
-    $controller = new $controllerName();
+    // Verifica se a classe do controlador existe
+    if (class_exists($controllerName)) {
 
-    // Chama a action apropriada no controlador
-    $controller->{$action}();
+        // Cria o objeto do controlador com base no nome do controlador
+        $controller = new $controllerName();
+
+        // Verifica se o método da action existe na classe do controlador
+        if (method_exists($controller, $action)) {
+            // Chama a action apropriada no controlador
+            $controller->{$action}();
+        } else {
+            // A action especificada não existe no controlador
+            require_once('views/404.php');
+        }
+    } else {
+        // A classe do controlador especificada não existe
+        require_once('views/404.php');
+    }
 } else {
     // Rota não encontrada para o método HTTP especificado
     require_once('views/404.php');
 }
-?>
+
 
 
